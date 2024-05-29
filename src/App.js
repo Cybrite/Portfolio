a5a9079613fd61fd1411266de06ddd75e364e40c
Harsh <140698710+Cybrite@users.noreply.github.com>
2024-05-29 22:51:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -140,7 +140,7 @@ function Intro() {
           <em>This portfolio is made using React.js and Tailwind CSS.</em>
         </p>
         <p>
-          if i talk about Myself a passionate web developer with a flair for
+          If i talk about Myself a passionate web developer with a flair for
           creativity. My journey began with <b> Python</b> basics, shifting to
           <b> C++</b>, later diving into web development with
           <b> HTML,CSS & vanilla Javascript</b>. I'm currently mastering{" "}

bdb5a712cb7243b9c294ea4fa9cb9e12dadfc034
Harsh <harshtanishq2002@gmail.com>
2024-05-30 13:54:21 +0530

    cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,24 +8,14 @@ module.exports = {
       },
       animation: {
         fadeIn: "fadeIn 3s linear",
-        bounce: "bounce 1s linear",
       },
       keyframes: {
         fadeIn: {
           "0%": { opacity: 0 },
           "100%": { opacity: 1 },
         },
-        bounce: {
-          "0%, 100%": {
-            transform: " translateY(0)",
-          },
-          "50%": {
-            transform: "translate(-20px)",
-          },
-        },
       },
     },
   },
   plugins: [],
-}
-
+};
